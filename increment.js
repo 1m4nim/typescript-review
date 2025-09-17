ba2978d8fe1032eada4668cf28ab4b489e074c50
1m4nim <fancustard9@gmail.com>
2025-09-17 10:33:14 +0900

    簡単な関数を作ろう終わり。

---
+++ increment.js
@@ -0,0 +1,9 @@
+// function increment(num:number){
+//     return num + 1;
+// }
+// console.log(increment("999"));
+//引数にはnumberしか入れられないはずなのに"999"のstringは許されるのか？
+function increment2(num) {
+    return num + 1;
+}
+console.log(increment2(999));
